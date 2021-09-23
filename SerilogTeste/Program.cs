8463189203e20970275ada4822c39e1a0e73230a
Jonathan <jonathan.vieira@economia.df.gov.br>
2021-09-23 13:31:29 -0300

    add(Serilog.AspNetCore e Serilog.Sinks.MSSqlServer)

--- SerilogTeste/Program.cs
+++ SerilogTeste/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +19,22 @@ namespace SerilogTeste
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var configurationRoot = config.Build();
+
+                    Serilog.Log.Logger = new LoggerConfiguration()
+                        .Enrich.FromLogContext()
+                        .WriteTo.MSSqlServer(configurationRoot.GetConnectionString("SeriLog"),
+                            sinkOptions: new Serilog.Sinks.MSSqlServer.MSSqlServerSinkOptions
+                            {
+                                AutoCreateSqlTable = true,
+                                TableName = "Logs"
+                            })
+                        .CreateLogger();
+
+                })
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
